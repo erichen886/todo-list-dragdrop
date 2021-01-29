e4dfc94c0d54a814e5836e50ac66ede6e55b0a0c
erichen886 <erichen886@gmail.com>
2021-01-29 14:42:51 -0800

    basic mockup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 const App = () => {
   return(
     <div>
-      <h1>HI there now</h1>
+      <div className="list-gridcontainer">
+        <div className="title">Kanban Board</div>
+        <div className="add-task">add task</div>
+        <div className="add-in-progress"> add in progress</div>
+        <div className="add-completed"> add completed</div>
+        <div className="task-list">
+          <div className ="task-card">
+            card1
+          </div>
+        </div>
+        <div className="in-progress-list">
+          <div>card2</div>
+        </div>
+        <div className="completed-list">
+          <div>card3</div>
+        </div>
+      </div>
     </div>
   )
 }
